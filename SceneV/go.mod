fb0ce5339f50f99a41d6e5faf5ccbade4d3ec8ac
vili1120 <vili1120@localhost.my.domain>
2025-05-07 19:00:51 +0200

    Created repo

---
+++ SceneV/go.mod
@@ -0,0 +1,11 @@
+module SceneV
+
+go 1.24.2
+
+require github.com/gen2brain/raylib-go/raylib v0.0.0-20250504022611-e6017e5fc409
+
+require (
+	github.com/ebitengine/purego v0.7.1 // indirect
+	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+)
---
+++ SceneV/main.go
@@ -0,0 +1,25 @@
+package scenev
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+type Game interface {
+  Draw()
+  Update() error
+  Layout() (int, int)
+}
+
+func RunGame(g Game) error {
+  Width, Height := g.Layout()
+  rl.InitWindow(int32(Width), int32(Height), "test")
+  defer rl.WindowShouldClose()
+
+  for !rl.WindowShouldClose() {
+    if err := g.Update(); err != nil {
+      return err
+    }
+    rl.BeginDrawing()
+    g.Draw()
+    rl.EndDrawing()
+  }
+  return nil
+}
