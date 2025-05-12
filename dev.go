939402e80ee74edd7c118baeb6f3801bbb80fff9
vili1120 <vili1120@localhost.my.domain>
2025-05-12 18:35:41 +0200

    Created repo

---
+++ dev.go
@@ -0,0 +1,13 @@
+package scenev
+
+func InitMSG(prompt string) {
+  println("[INIT] "+prompt)
+}
+
+func SuccessMSG(prompt string) {
+  println("[SUCCESS] "+prompt)
+}
+
+func ErrorMSG(prompt string) {
+  println("[ERROR] "+prompt)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/vili1120/Engine
+
+go 1.24.3
+
+require (
+	github.com/veandco/go-sdl2 v0.4.40 // indirect
+	github.com/vulkan-go/vulkan v0.0.0-20221209234627-c0a353ae26c8 // indirect
+)
